f8c2db9067e0501a49478d7de6055ad0780eea4f
EnzoLFranco <fenzolemos@gmail.com>
2025-05-16 15:54:54 -0300

    feat: Cria o método para gerar ID e remove comentários

--- Lab 3/API/cardapi/src/main/java/com/enzolfranco/cardapi/cardController.java
+++ Lab 3/API/cardapi/src/main/java/com/enzolfranco/cardapi/cardController.java
@@ -9,48 +9,55 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-@RestController // Define que essa classe vai ser um controlador
-@RequestMapping("/cards") // Define o caminho base para os endpoints
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+@RestController
+@RequestMapping("/cards")
 public class cardController {
     private List<card> cards = new ArrayList<>();
 
-    @PostConstruct // Anotação que indica que esse metodo sera executado após o construtor da classe ser chamado
+    private String generateId() {
+        int nextId = cards.size() + 1;
+        return String.format("CARD-%04d", nextId);
+    }
+
+    @PostConstruct
     public void init() {
-        // Adiciona dois cards à lista quando o controlador é inicializado
-        cards.add(new card("1", "Cavaleiro A", "Guerreiro", "O melhor guerreiro"));
-        cards.add(new card("2", "Mago B", "Mago", "O mago mais sábio"));
+        cards.add(new card("CARD-0001", "Cavaleiro A", "Guerreiro", "O melhor guerreiro"));
+        cards.add(new card("CARD-0002", "Mago B", "Mago", "O mago mais sábio"));
     }
 
-    @GetMapping // Define o endpoint HTTP GET para recuperar todos os cards
+    @GetMapping
     List<card> getCards(){
-        return cards; // Retorna a lista de cards
+        return cards;
     }
 
-    @GetMapping("/{id}") // Define o endpoint GET para recuperar um card específico com base no id
+    @GetMapping("/{id}")
     Optional<card> getCardById(@PathVariable String id){
         for(card cd: cards){
-            if(cd.getId().equals(id)){  // Se encontrar o card, retorna ele embrulhado em um Optional
+            if(cd.getId().equals(id)){
                 return Optional.of(cd);
             }
         }
-        return Optional.empty(); // Se não encontrar, retorna um Optional vazio
+        return Optional.empty();
     }
 
-    @PostMapping // Define o endpoint POST para criar um novo card
+    @PostMapping
     card createCard(@RequestBody card card){
-        cards.add(card); // Adiciona o novo card à lista
-        return card; // Retorna o card que foi criado
+        card.setId(generateId());
+        cards.add(card);
+        return card;
     }
 
-    @PutMapping("/{id}") // Define o endpoint PUT para atualizar um card existente com base no id
+    @PutMapping("/{id}")
     ResponseEntity<card> updateCard(@PathVariable String id, @RequestBody card card) {
         int cardIndex = -1;
 
         for (card c: cards) {
-            if (c.getId().equals(id)) { // Se o id for encontrado, atualiza o card na lista
-                cardIndex = cards.indexOf(c);  // Atualiza o índice do card
-                cards.set(cardIndex, card);  // Substitui o card antigo pelo novo
+            if (c.getId().equals(id)) {
+                cardIndex = cards.indexOf(c);
+                cards.set(cardIndex, card);
             }
         }
 
@@ -59,9 +66,8 @@ public class cardController {
                 new ResponseEntity<>(card, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}") // Define o endpoint DELETE para excluir um card com base no id
-    public void deleteCard(@PathVariable String id) {  // Recebe o id do card a ser excluído
-        // Remove o card da lista com base no id
+    @DeleteMapping("/{id}")
+    public void deleteCard(@PathVariable String id) {
         cards.removeIf(c -> c.getId().equals(id));
     }
 
